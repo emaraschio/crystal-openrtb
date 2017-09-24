3ab0c7dfe81c529507181093ad47ea4e310660e9
Ezequiel Maraschio <ezequiel.maraschio@gmail.com>
2017-09-24 18:43:59 -0300

    Add errors and readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Crystal OpenRTB v2.x
+
+OpenRTB implementation for Crystal
+
+## Installation
+
+## Usage
+
+## License
+
+## Samples
+
+Some test examples were taken from:
+https://code.google.com/p/openrtb/wiki/OpenRTB_Examples
---
+++ src/rtb/errors.cr
@@ -0,0 +1,23 @@
+module RTB
+  class Error < ::Exception; end
+
+  #Bid Errors
+  class InvalidBidWithoutIdError < Error; end
+
+  class InvalidBidWithoutImpressionsError < Error; end
+
+  #Bid Request Errors
+  class InvalidBidRequestWithoutIdError < Error; end
+
+  class InvalidBidRequestWithoutImpressionsError < Error; end
+
+  #Bid Responses Errors
+  class InvalidBidResponseWithoutIdError < Error; end
+
+  class InvalidBidResponseWithoutSeatBidsError < Error; end
+
+  # 5.19 No-Bid Reason Codes
+  class NoBidReasonUnknownError < Error; end
+
+  class NoBidReasonTechnicalError < Error; end
+end
